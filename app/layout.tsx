ff8728a0b330a364c7c8e56719aee192cee69188
turltetuber <mamatoya@asu.edu>
2025-01-02 13:22:03 -0700

    cleaned it up, maybe mobile friendly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,17 @@
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Grandstander } from 'next/font/google'
 
-const montserrat = Montserrat({
-    subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
-    variable: '--font-montserrat',
-})
-
 const grandstander = Grandstander({
     subsets: ['latin'],
-    weight: ['700', '800'],
+    weight: ['700', '800']
 })
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
     subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
     subsets: ["latin"],
 });
 
@@ -34,7 +26,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className={`${grandstander.className}`}>
+        <html lang="en" className={`${grandstander.className} ${geistSans.className} ${geistMono.className}`}>
         <body>{children}</body>
         </html>
     )
